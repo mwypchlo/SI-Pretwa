b0728af85a595871ae38a6b4c66480e164651b49
michalw <michal.wypchlo@vp.pl>
2019-11-19 19:14:47 +0100

    External game window + board prototype

---
+++ SI-PretwaGUI/Game.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SI_PretwaGUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Game.xaml
+    /// </summary>
+    public partial class Game : Window
+    {
+
+
+        public Game()
+        {
+
+
+
+            InitializeComponent();
+        }
+
+    }
+}
