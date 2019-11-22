53012d2152024359d8672c0dbdb5a3b8ab3e1e05
michalw <michal.wypchlo@vp.pl>
2019-11-22 15:11:09 +0100

    Pawn statuses added

---
+++ SI-PretwaGUI/PawnStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI_PretwaGUI
+{
+    class PawnStatus
+    {
+        [Flags]
+        public enum FieldDrawType
+        {
+            Default = 0,
+            Highlighted = 1,
+            Selected = 2
+        }
+    }
+}
