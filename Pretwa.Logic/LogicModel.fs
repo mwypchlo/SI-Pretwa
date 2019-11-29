8d545c913075426f9d7a4c38c5e1b02b9bbb6684
michalw <michal.wypchlo@vp.pl>
2019-11-29 22:30:38 +0100

    Created F# project for game logic

---
+++ Pretwa.Logic/LogicModel.fs
@@ -0,0 +1,12 @@
+﻿namespace Pretwa.Logic
+
+type public Player =
+    | Blue
+    | Red
+
+type public FieldState =
+    | Color of Player
+    | Empty
+
+type public NextMove =
+    | Color of Player
