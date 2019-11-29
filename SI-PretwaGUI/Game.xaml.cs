75907ac045731dc510ecefc0d80ee1cfb859ea52
michalw <michal.wypchlo@vp.pl>
2019-11-29 18:34:25 +0100

    deleted usless code

--- SI-PretwaGUI/Game.xaml.cs
+++ SI-PretwaGUI/Game.xaml.cs
@@ -19,17 +19,6 @@ namespace SI_PretwaGUI
     public partial class Game : Window
     {
         public List<System.Windows.Point> BoardPoints = new List<System.Windows.Point>();
-    
-
-        List<Grid> czerwone = new List<Grid>();
-        List<Grid> zolte = new List<Grid>();
-        private Boolean pierwszyKlik; //kazdy ruch to dwa klikniecia - zrodlo i cel
-        private Boolean blokujPionek; //zmusza gracza do gry jednym pionkiem (wielokrotne bicie)
-        private Boolean ruchWykonany;
-        private int[] mv, xy, blokowany;
-        private System.Windows.Controls.Button sndr, tempBtn;
-        private DoubleAnimation fadeIn;
-        private double anlen;
         private void FillGameBoard()
         {
             BoardPoints.Add(new System.Windows.Point(400, 401)); //0
@@ -68,7 +57,7 @@ namespace SI_PretwaGUI
         private void ResButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-           Game reset = new Game();
+            Game reset = new Game();
             reset.Show();
         }
 
